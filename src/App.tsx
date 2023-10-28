10d303b7b80abfb51bc7155ee4e520fa0a2b7ad7
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2023-10-28 14:03:44 +0200

    Get rid of local icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,9 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
+import { Action, actions, pages } from './Data';
 
-interface Page {
-  name: string;
-  icon: string;
-}
-
-interface Action {
-  name: string;
-  icon: string;
-}
-const pages: Page[] = [
-  { name: 'Media', icon: '/icons/wrap.png' },
-  { name: 'OBS', icon: '/icons/wrench-check-outline.png' },
-  { name: 'Youtube', icon: '/icons/wrench-check.png' },
-  { name: 'Premiere', icon: '/icons/wrench-clock-outline.png' },
-  { name: 'Chrome', icon: '/icons/wrench-clock.png' },
-  { name: 'Photoshop', icon: '/icons/wrench-cog-outline.png' },
-  // Add more pages with their corresponding icons
-];
-
-const actions: { [pageName: string]: Action[] } = {
-  Media: [
-    { name: 'Volume Up', icon: '/icons/wrench.png' },
-    { name: 'Volume Down', icon: '/icons/xamarin.png' },
-    { name: 'Next', icon: '/icons/xml.png' },
-    { name: 'Play/Pause', icon: '/icons/xmpp.png' },
-    { name: 'Previous', icon: '/icons/yahoo.png' },
-    { name: 'Mute', icon: '/icons/yeast.png' },
-  ],
-  OBS: [
-    { name: 'Start Streaming', icon: '/icons/yin-yang.png' },
-    { name: 'Stop Streaming', icon: '/icons/yoga.png' },
-    { name: 'Start Recording', icon: '/icons/youtube-gaming.png' },
-    { name: 'Stop Recording', icon: '/icons/youtube-studio.png' },
-    // Add more OBS actions here
-  ],
-  Youtube: [
-    { name: 'Like', icon: '/icons/youtube-subscription.png' },
-    { name: 'Dislike', icon: '/icons/youtube-tv.png' },
-    { name: 'Subscribe', icon: '/icons/youtube.png' },
-    { name: 'Share', icon: '/icons/yurt.png' },
-    // Add more Youtube actions here
-  ],
-  Premiere: [
-    { name: 'Cut', icon: '/icons/z-wave.png' },
-    { name: 'Copy', icon: '/icons/zend.png' },
-    { name: 'Paste', icon: '/icons/zigbee.png' },
-    { name: 'Undo', icon: '/icons/zip-box-outline.png' },
-    // Add more Premiere actions here
-  ],
-  Chrome: [
-    { name: 'Open New Tab', icon: '/icons/zip-box.png' },
-    { name: 'Close Tab', icon: '/icons/zip-disk.png' },
-    { name: 'Refresh Page', icon: '/icons/zodiac-aquarius.png' },
-    { name: 'Bookmark Page', icon: '/icons/zodiac-aries.png' },
-    // Add more Chrome actions here
-  ],
-  Photoshop: [
-    { name: 'Create New Layer', icon: '/icons/zodiac-cancer.png' },
-    { name: 'Select Area', icon: '/icons/zodiac-capricorn.png' },
-    { name: 'Apply Filter', icon: '/icons/zodiac-gemini.png' },
-    { name: 'Undo', icon: '/icons/zodiac-leo.png' },
-    // Add more Photoshop actions here
-  ],
-};
+const mdIcoUrl =
+  'https://raw.githubusercontent.com/Templarian/MaterialDesign/master/svg/';
 
 interface PageItemProps {
   name: string;
@@ -92,7 +31,7 @@ function PageContent({ pageName, actions }: PageContentProps) {
       <div className="grid grid--action">
         {actions.map((action, index) => (
           <div className="action icon" key={index}>
-            <img src={action.icon} alt={action.name} />
+            <img src={`${mdIcoUrl}${action.icon}.svg`} alt={action.name} />
             <p>{action.name}</p>
           </div>
         ))}
@@ -112,7 +51,11 @@ function App() {
         <section className="grid">
           {pages.map((page, index) => (
             <Link key={index} to={`/page/${page.name}`}>
-              <PageItem key={index} name={page.name} icon={page.icon} />
+              <PageItem
+                key={index}
+                name={page.name}
+                icon={`${mdIcoUrl}${page.icon}.svg`}
+              />
             </Link>
           ))}
         </section>
